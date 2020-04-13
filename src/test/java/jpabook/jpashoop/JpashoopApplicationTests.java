bddb0823eb015a8e6bc280850f1d34006e608cd7
lua928908 <47199153+lua928908@users.noreply.github.com>
2020-04-14 00:24:56 +0900

    초기설정 세팅
    start.spring.io 에서 초기설정 가능

---
+++ src/test/java/jpabook/jpashoop/JpashoopApplicationTests.java
@@ -0,0 +1,16 @@
+package jpabook.jpashoop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpashoopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
