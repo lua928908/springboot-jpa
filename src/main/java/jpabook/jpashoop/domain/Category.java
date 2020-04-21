463aba7753a77ed280ac0406dc7604bb597794f9
lua928908 <47199153+lua928908@users.noreply.github.com>
2020-04-21 23:25:25 +0900

    엔티티 설계시 주의점
    1. N:M 쓰지않기
    2. 즉시로딩대신 지연로딩쓰기 (LAZY)
    3. setter 최대한 만들지 말기
    4. 테이블명 바꾸고싶으면 spring.jpa.hibernate.naming.implicit-strategy 와 spring.jpa.hibernate.naming.physical-strategy 수정하기
    5. 양방향 관계인 경우 연관관계편의메서드 만들어주기
    6. 컬렉션은 필드에서 초기화하고 변경하지 않기 ( new ArrayList<>() )

--- src/main/java/jpabook/jpashoop/domain/Category.java
+++ src/main/java/jpabook/jpashoop/domain/Category.java
@@ -25,10 +25,16 @@ public class Category {
             inverseJoinColumns = @JoinColumn(name = "item_id")) // 중간테이블과 매핑해주어야 함 (중간테이블을 만들고 중간테이블이 1:N, N:1 으로 풀어냄, 실무에서는 필드추가가 안되서 사용불가)
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    // 연관관계 편의 메서드
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
