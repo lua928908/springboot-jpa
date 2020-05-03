a68152a2efff1417811074af3ffcd6077b4ee8a0
lua928908 <47199153+lua928908@users.noreply.github.com>
2020-05-03 12:27:17 +0900

    상품 등록, 삼품 수정, merge를 변경 감지로 변경

--- src/main/java/jpabook/jpashoop/service/ItemService.java
+++ src/main/java/jpabook/jpashoop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashoop.service;
 
+import jpabook.jpashoop.domain.Item.Book;
 import jpabook.jpashoop.domain.Item.Item;
 import jpabook.jpashoop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,14 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity){
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems(){
