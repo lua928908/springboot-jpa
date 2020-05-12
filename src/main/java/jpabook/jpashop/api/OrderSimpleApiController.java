77e45f5e5913714d2e229e8ef2e299a89f8260f7
lua928908 <47199153+lua928908@users.noreply.github.com>
2020-05-12 23:46:11 +0900

    엔티티를 DTO로 변환
    엔티티를 DTO로 변환했으나 조회해온 주문엔티티를 DTO로 변환하는 과정에서 getName(), getAddress()로 인해 LAZY 로드때문에 n+1 문제가 발생한다.
    
    이 문제를 해결하기 위해 fetch join을 활용해야 한다.

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +23,32 @@ public class OrderSimpleApiController {
     public List<Order> oversV1(){
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         return all; // 무한루프에 빠짐, order를 json으로 뿌려야하는데 order안에 member가 있고 member안에 다시 order가 있기 때문이다. @JsonIgnore 해야함
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2(){
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        System.out.println("orders = " + orders);
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o)) // map에서 멤버name과 딜리버리 address를 가져올때 n+1 쿼리 문제 발생함. (해결하기 위해 fetch join 필요)
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
