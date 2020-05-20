6bc65317e8676952e43ed63ccf614c679ae82abb
lua928908 <47199153+lua928908@users.noreply.github.com>
2020-05-21 00:09:02 +0900

    V6 숨김 처리, spring.jpa.open-in-view
    application.yeml spring.jpa.open-in-view 값 기본이 true

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -81,16 +81,16 @@ public class OrderApiController {
         return orderQueryRepository.findAllByDto_optimization();
     }
 
-    @GetMapping("/api/v6/orders")
-    public List<OrderQueryDto> ordersV6() {
-        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
-        return flats.stream()
-                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
-                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
-                )).entrySet().stream()
-                .map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue()))
-                .collect(toList());
-    }
+//    @GetMapping("/api/v6/orders")
+//    public List<OrderQueryDto> ordersV6() {
+//        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+//        return flats.stream()
+//                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+//                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+//                )).entrySet().stream()
+//                .map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue()))
+//                .collect(toList());
+//    }
 
     @Getter
     static class OrderDto{
