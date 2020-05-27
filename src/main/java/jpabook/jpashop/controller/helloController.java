7d063f5cb2e3df7a2e56b0f182bc78de710cdf1f
lua928908 <47199153+lua928908@users.noreply.github.com>
2020-05-27 22:44:46 +0900

    임시 커밋

---
+++ src/main/java/jpabook/jpashop/controller/helloController.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
