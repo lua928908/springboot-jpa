79e624760b11d48169ab9f174b960e733759d179
lua928908 <47199153+lua928908@users.noreply.github.com>
2020-06-03 00:29:01 +0900

    메소드 이름으로 쿼리 생성

--- src/main/java/jpabook/jpashop/entity/Member.java
+++ src/main/java/jpabook/jpashop/entity/Member.java
@@ -22,6 +22,15 @@ public class Member {
     @JoinColumn(name = "team_id")
     private Team team;
 
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
     public Member(String username, int age, Team team) {
         this.username = username;
         this.age = age;
--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
