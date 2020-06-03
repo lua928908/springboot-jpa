029e97eb743b9a25030696aa082cef51633fc5d1
lua928908 <47199153+lua928908@users.noreply.github.com>
2020-06-04 00:56:25 +0900

    @Query를 통한 query문 직접 입력
    namedQuery는 실무에서 사실상 거의 사용하지 않음, namedQuery의 장점과 단점도 극복한 @Query를 사용한다.
    
    인터페이스에 메소드위에 @Query어노테이션을 붙이고 바로 JPQL 쿼리를 작성해 사용 가능.

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -2,9 +2,14 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
--- src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -75,4 +75,15 @@ class MemberRepositoryTest {
         assertThat(result.get(0).getAge()).isEqualTo(20);
         assertThat(result.size()).isEqualTo(1);
     }
+
+    @Test
+    public void testQuery(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+        assertThat(result.get(0)).isEqualTo(m1);
+    }
 }
