45d1e596526c541ecbe76e9e4007ebf689742551
lua928908 <47199153+lua928908@users.noreply.github.com>
2020-06-20 19:20:05 +0900

    queryDSL의 기본문법
    진짜 좋긴 좋은거 같다.

--- src/test/java/jpabook/jpashop/QuerydslBasicTest.java
+++ src/test/java/jpabook/jpashop/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.entity.Member;
 import jpabook.jpashop.entity.QMember;
@@ -12,6 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+
+import java.util.List;
 
 import static jpabook.jpashop.entity.QMember.*;
 import static org.assertj.core.api.Assertions.*;
@@ -63,5 +66,81 @@ public class QuerydslBasicTest {
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void search(){
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1")
+                                .and(member.age.eq(10))
+                )  // 이름이 10살 이면서 나이가 10살인 사람
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void searchAndParam(){
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10) // .and()로 체인을 쓰는것 대신 콤마로 첫,두번째 인자로 넘길수도 있다. (하지만 and만 가능하고 or는 체인으로 해줘야함)
+                )
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void resultFetch(){
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        Member fetchOne = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+
+        Member fetchFirst = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+
+        QueryResults<Member> results = queryFactory // 페이징으로 가져오는것이여서 data와 count를 가져올 수 있다.
+                .selectFrom(member)
+                .fetchResults();
+        results.getTotal();
+        List<Member> content = results.getResults();
+
+        long total = queryFactory // count갯수만 가져옴
+                .selectFrom(member)
+                .fetchCount();
+    }
+
+
+    /*
+    * 1. 회원 나이 내림차순(DESC)
+    * 2. 회웜 이름 올림차순(ASC)
+    * 단 2에서 회원 이름이 없으면 마지막에 출력 (nulls last)
+    * */
+    @Test
+    public void sort(){
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
     }
 }
